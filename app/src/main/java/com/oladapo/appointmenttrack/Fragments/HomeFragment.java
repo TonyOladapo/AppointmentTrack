0dbde65b3aad3275bb46d03378974362bae0974d
oladapo <toneyobaze@gmail.com>
2019-07-11 20:16:26 +0000

    changed to firebase authentication with Google

---
+++ app/src/main/java/com/oladapo/appointmenttrack/Fragments/HomeFragment.java
@@ -0,0 +1,34 @@
+package com.oladapo.appointmenttrack.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.oladapo.appointmenttrack.R;
+
+import java.util.Objects;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Objects.requireNonNull(getActivity()).setTitle(R.string.app_name);
+    }
+}
--- app/src/main/java/com/oladapo/appointmenttrack/MainActivity.java
+++ app/src/main/java/com/oladapo/appointmenttrack/MainActivity.java
@@ -1,40 +1,78 @@
 package com.oladapo.appointmenttrack;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import java.util.Arrays;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.oladapo.appointmenttrack.Fragments.HomeFragment;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
-    public static final int RC_SIGN_IN = 1;
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleApiClient;
+
+    private static final String TAG = "vkv";
+
+    private PrimaryDrawerItem home, logout, login;
+    private Drawer drawer;
+    private AccountHeader header;
+
+    private CoordinatorLayout coordinatorLayout;
+    private Toolbar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        initToolbar();
+
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
 
         mAuth = FirebaseAuth.getInstance();
+
+        initDrawer();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,34 +83,204 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        PrimaryDrawerItem home = new PrimaryDrawerItem();
+        signIn();
+    }
 
-        new DrawerBuilder()
-                .withActivity(this)
-                .addDrawerItems(home)
-                .withActionBarDrawerToggleAnimated(true)
+    private void signInWithGoogle() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
                 .build();
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount acct = result.getSignInAccount();
+
+                linkWithGoogle(acct);
+            } else {
+                Snackbar.make(coordinatorLayout, "Authentication failed!", Snackbar.LENGTH_LONG).show();
+                Log.i(TAG, result.toString());
+            }
+        }
+    }
+
+    private void linkWithGoogle(final GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        Objects.requireNonNull(mAuth.getCurrentUser()).linkWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = Objects.requireNonNull(task.getResult()).getUser();
+                            updateUI(user);
+                            Snackbar.make(coordinatorLayout, "signed is as " + user.getDisplayName(), Snackbar.LENGTH_LONG).show();
+
+                        } else {
+                            Exception exception = task.getException();
+                            if (exception instanceof FirebaseAuthUserCollisionException) {
+                                firebaseAuthWithGoogle(acct);
+
+                            } else {
+                                Snackbar.make(coordinatorLayout, "Authentication failed!", Snackbar.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = Objects.requireNonNull(task.getResult()).getUser();
+                            updateUI(user);
+
+                            Snackbar.make(coordinatorLayout, "signed is as " + user.getDisplayName(), Snackbar.LENGTH_LONG).show();
+                        } else {
+
+                            Snackbar.make(coordinatorLayout, "Authentication failed!", Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void signIn() {
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
+
                 if (user != null) {
 
                 } else {
-                    startActivityForResult(
-                            AuthUI.getInstance()
-                                    .createSignInIntentBuilder()
-                                    .setIsSmartLockEnabled(true)
-                                    .setAvailableProviders(Arrays.asList(
-                                            new AuthUI.IdpConfig.EmailBuilder().build(),
-                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
-                                            new AuthUI.IdpConfig.AnonymousBuilder().build()))
-                            .build(),
-                            RC_SIGN_IN);
+                    mAuth.signInAnonymously()
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Snackbar.make(coordinatorLayout, "You are signed in with an anonymous account", Snackbar.LENGTH_LONG)
+                                                .setAction("action", null).show();
+
+                                        new Handler().postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                linkAccountAlertDialog();
+                                            }
+                                        }, 3000);
+                                    } else {
+                                        Log.d(TAG, "signInAnonymously", task.getException());
+                                        Snackbar.make(coordinatorLayout, "Authentication failed!", Snackbar.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
                 }
             }
         };
+    }
+
+    private void updateUI(FirebaseUser user) {
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+        Uri photoUri = user.getPhotoUrl();
+
+        ProfileDrawerItem userProfile = new ProfileDrawerItem()
+                .withName(name)
+                .withEmail(email)
+                .withIcon(photoUri);
+
+        header.updateProfile(userProfile);
+    }
+
+    private void linkAccountAlertDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Would you like to link your Google account?")
+                .setMessage("You are currently signed in as an anonymous user and in the " +
+                        "instance of losing access to this account by uninstalling the app or clearing app data," +
+                        " you would not regain access to the account. The only way to get back your account is by linking " +
+                        "your Google account to avoid losing your data.")
+                .setPositiveButton("Link Google account", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        linkWithGoogle();
+                        signInWithGoogle();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Do something
+                    }
+                })
+                .show();
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimaryDark));
+    }
+
+    private void initDrawer() {
+        login = new PrimaryDrawerItem()
+                .withIdentifier(1)
+                .withTextColorRes(R.color.colorPrimaryDark)
+                .withSelectedTextColorRes(R.color.colorPrimary)
+                .withSelectedColorRes(R.color.colorPrimaryDark);
+
+        home = new PrimaryDrawerItem()
+                .withIdentifier(2)
+                .withTextColorRes(R.color.colorPrimaryDark)
+                .withSelectedTextColorRes(R.color.colorPrimary)
+                .withSelectedColorRes(R.color.colorPrimaryDark);
+
+        header = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withCompactStyle(true)
+                .addProfiles(
+                        new ProfileDrawerItem()
+                )
+                .withTranslucentStatusBar(true)
+                .build();
+
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(header)
+                .addDrawerItems(home)
+                .withSelectedItem(-1)
+                .withActionBarDrawerToggleAnimated(true)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        long i = drawerItem.getIdentifier();
+
+                        if (i == 2) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+                                    .addToBackStack(null)
+                                    .replace(R.id.layout_container, new HomeFragment())
+                                    .commit();
+                        }
+                        return false;
+                    }
+                })
+                .build();
     }
 
     @Override
@@ -106,6 +314,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
         mAuth.addAuthStateListener(mAuthStateListener);
+        if (user != null && !user.isAnonymous()) {
+            updateUI(user);
+            Log.i(TAG, user.getDisplayName());
+        }
     }
 }
